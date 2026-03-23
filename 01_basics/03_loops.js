e83443a0edcf9db346571ad6f04f23e337ab9292
@prateekpandey <prateekpandey707@gmail.com>
2026-03-23 09:54:53 +0000

    loops done

---
+++ 01_basics/03_loops.js
@@ -0,0 +1,19 @@
+// for loop syntax
+
+// for (let i = 0; i <= 10; i++) {
+//     const result = i;
+//     if (result === 5) {
+//         console.log("5 is the best No");
+        
+//     }
+//     console.log(result);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+    
+//     for (let j = 0; j <= 10; j++) {
+//         // console.log(`inner loop value: ${i} and outer loop value ${j}` );
+//         console.log(i + `*` + j + `=` + i*j);
+//     }
+// }
